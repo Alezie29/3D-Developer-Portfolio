0ccdc08f69140e50c2b0577ca93e6aa4ab8f63e1
Alex Mckenzie <86716054+Alezie29@users.noreply.github.com>
2023-06-28 16:06:00 +0700

    upload to hostinger and complete!

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,15 +10,58 @@ import { slideIn } from '../utils/motion';
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
-    name: '',
-    email: '',
-    message: '',
+    name: "",
+    email: "",
+    message: "",
   });
+  
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {}
+  const handleChange = (e) => {
+    const { target } = e;
+    const { name, value } = e.target;
+    
+    setForm({ 
+      ...form, 
+      [name]: value
+    });
+  };
 
-  const handleSubmit = (e) => {}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+      'service_1smfpmg', 
+      'template_9g7zz8p',
+      {
+        from_name: form.name,
+        to_name: "Alex",
+        from_email: form.email,
+        to_email: "alexmckenzie2902@gmail.com",
+        message: form.message,
+      },
+      'bIMyOQygwCi0ftIJg'
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert('Thank you. I will get back to you as soon as possible!');
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        }, 
+        (error) => {
+          setLoading(false)
+          console.log(error);
+
+          alert('Something went wrong.')
+        }); 
+  };
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
@@ -27,7 +70,7 @@ const Contact = () => {
         className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
       >
         <p className={styles.sectionSubText}>Get in touch</p>
-        <h3 className={styles.sectionHeadText}>Get in touch</h3>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
 
         <form
           ref={formRef}
@@ -61,9 +104,9 @@ const Contact = () => {
             <textarea
               rows="7"
               name="message"
-              value={form.name}
+              value={form.message}
               onChange={handleChange}
-              placeholder="What's your name?"
+              placeholder="What's your message?"
               className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
             />
           </label>
@@ -83,9 +126,8 @@ const Contact = () => {
       >
         <EarthCanvas />
       </motion.div>
-
     </div>
-  )
-}
+  );
+};
 
-export default SectionWrapper(Contact, "contact") 
+export default SectionWrapper(Contact, "contact"); 
